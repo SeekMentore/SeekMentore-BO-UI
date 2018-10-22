092d25fccc7f869b465688d3c6e3265334128253
Shantanu-Mukherjee <37309507+Shantanu-Mukherjee@users.noreply.github.com>
2018-10-21 23:26:32 -0500

    Update src/app/login-controlled-pages/employee-home/employee-home.component.ts

--- src/app/login-controlled-pages/employee-home/employee-home.component.ts
+++ src/app/login-controlled-pages/employee-home/employee-home.component.ts
@@ -78,7 +78,7 @@ export class EmployeeHomeComponent implements OnInit, AfterViewInit {
       'Initiate Date': (new Date(record.getProperty('initiatedDateMillis'))).toDateString(),
       'Subject': record.getProperty('subject'),
       'Initiated By': record.getProperty('initiatedBy'),
-      'Due Date': new Date(record.getProperty('dueDateMillis')),
+      'Due Date': (new Date(record.getProperty('dueDateMillis'))).toDateString(),
       'Action Date': (new Date(record.getProperty('actionDateMillis'))).toDateString(),
       'Action By': record.getProperty('actionBy')
     };

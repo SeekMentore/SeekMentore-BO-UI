251f30359c00d6c1824105dc4c4f3a94701642d7
Shantanu-Mukherjee <37309507+Shantanu-Mukherjee@users.noreply.github.com>
2018-10-25 22:45:11 -0500

    Update src/app/utils/routing.module.ts

--- src/app/utils/routing.module.ts
+++ src/app/utils/routing.module.ts
@@ -31,8 +31,8 @@ const routes: Routes = [
       {path: '**', redirectTo: 'login', pathMatch: 'full'}
     ]
   },
-  {path: '', redirectTo: 'public', pathMatch: 'full'},
-  {path: '**', redirectTo: 'public', pathMatch: 'full'}
+  {path: '', redirectTo: '/public/login', pathMatch: 'full'},
+  {path: '**', redirectTo: '/public/login', pathMatch: 'full'}
 ];
 
 

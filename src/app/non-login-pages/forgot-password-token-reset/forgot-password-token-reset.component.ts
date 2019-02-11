2494c8b8528716bbb94a1b42570bdeb2bc69ab89
Shantanu-Mukherjee <37309507+Shantanu-Mukherjee@users.noreply.github.com>
2019-02-11 15:58:31 -0600

    Modified

--- src/app/non-login-pages/forgot-password-token-reset/forgot-password-token-reset.component.ts
+++ src/app/non-login-pages/forgot-password-token-reset/forgot-password-token-reset.component.ts
@@ -14,6 +14,7 @@ import { NlpRestUrls } from 'src/app/utils/nlp-rest-urls';
 export class ForgotPasswordTokenResetComponent implements OnInit {
 
   token: string;
+  tokenId: string;
   errorAjaxResponse: string;
   successMessage: string;
   errorNewPassword: string;
@@ -23,6 +24,7 @@ export class ForgotPasswordTokenResetComponent implements OnInit {
 
   constructor(private helperService: HelperService, private route: ActivatedRoute, private utilityService: AppUtilityService) {
     this.route.params.subscribe(params => {  
+      this.tokenId = params['tokenId'];
       this.token = params['token'];
       console.log(this.token);
     });
@@ -38,6 +40,7 @@ export class ForgotPasswordTokenResetComponent implements OnInit {
       return;
     }
     const formData = new URLSearchParams();
+    formData.set('tokenId', this.tokenId);
     formData.set('token', this.token);
     formData.set('newPassword', this.newPassword);
     formData.set('retypeNewPassword', this.retypeNewPassword);
--- src/app/utils/routing.module.ts
+++ src/app/utils/routing.module.ts
@@ -79,7 +79,7 @@ const routes: Routes = [
       {path: 'error', component: ErrorComponent},
       {path: 'login', component: LoginComponent},
       {path: 'resetpassword', component: ResetPasswordComponent},
-      {path: 'resetpassword/:token', component: ForgotPasswordTokenResetComponent},
+      {path: 'resetpassword/:tokenId/:token', component: ForgotPasswordTokenResetComponent},
       {path: '', redirectTo: 'login', pathMatch: 'full'},
       {path: '**', redirectTo: 'login', pathMatch: 'full'}
     ]

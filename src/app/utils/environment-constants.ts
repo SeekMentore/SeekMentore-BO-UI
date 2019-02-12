6c2aacf3bc0132d0ec4f12c5b850c9f5e7627eb1
Shantanu-Mukherjee <37309507+Shantanu-Mukherjee@users.noreply.github.com>
2019-02-12 11:45:01 -0600

    Update environment-constants.ts

--- src/app/utils/environment-constants.ts
+++ src/app/utils/environment-constants.ts
@@ -1,8 +1,37 @@
 export class EnvironmentConstants {
-  static SERVER_URL = 'http://localhost:8080';
+  /**
+   * PROD Settings
+   */
+  //static SERVER_URL = 'http://13.127.141.184:8080';
+  //static CONTEXT_PATH = '/seekmentore';
+  //static PUBLIC_PAGES_URL = 'http://www.seekmentore.com';
+  //static APPLICATION_NAME = 'SeekMentore – Back Office';
+  //static IMAGE_SERVER = 'http://imageserver.seekmentore.com';
+  /**
+   * PROD Settings
+   */
+
+  /**
+   * DEV Settings
+   */
   //static SERVER_URL = 'http://13.127.139.107:8080';
-  static PUBLIC_PAGES_URL = 'http://www.seekmentore.com';
+  //static CONTEXT_PATH = '/seekmentore';
+  //static PUBLIC_PAGES_URL = 'http://dev.seekmentore.com';
+  //static APPLICATION_NAME = 'SeekMentore (Dev) – Back Office';
+  //static IMAGE_SERVER = 'http://imageserver.seekmentore.com';
+  /**
+   * DEV Settings
+   */
+
+   /**
+   * LOCAL Settings
+   */
+  static SERVER_URL = 'http://localhost:8080';
   static CONTEXT_PATH = '/seekmentore';
-  static APPLICATION_NAME = 'SeekMentore (Dev) – Back Office';
+  static PUBLIC_PAGES_URL = 'http://localhost:8080/seekmentorepublicpages';
+  static APPLICATION_NAME = 'SeekMentore (Local) – Back Office';
   static IMAGE_SERVER = 'http://imageserver.seekmentore.com';
+  /**
+   * LOCAL Settings
+   */ 
 }

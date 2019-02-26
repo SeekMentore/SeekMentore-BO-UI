e3114b6d33406a85314a0eef6791a9df424ddb81
Shantanu-Mukherjee <37309507+Shantanu-Mukherjee@users.noreply.github.com>
2019-02-25 18:03:02 -0600

    Update lcp-rest-urls.ts

--- src/app/utils/lcp-rest-urls.ts
+++ src/app/utils/lcp-rest-urls.ts
@@ -54,5 +54,5 @@ export class LcpRestUrls {
   static subscription_package_update_record = '/rest/sales/updateSubscriptionPackageRecord';
   static take_action_on_subscription_package = '/rest/sales/takeActionOnSubscriptionPackage'; 
   static subscription_package_assignment_data_access = '/rest/sales/subscriptionPackageAssignmentCheckDataAccess'; 
-  static subscription_package_assignment_update_record = '/rest/sales/updatePackageAssignmentRecord';
+  static subscription_package_assignment_update_record = '/rest/sales/updateSubscriptionPackageAssignmentRecord';
 }
